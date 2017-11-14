e8032d13798c076ed77ba4cb49db15fdd6beb7f4
João Tiago Pereira Dias <joaotpd099@gmail.com>
2017-11-14 17:58:07 +0000

    Changes in new.c

--- Exemplos/new.c
+++ Exemplos/new.c
@@ -1,4 +1,4 @@
-void relax_jacobi (double *g2, double *g1, unsigned sizex, unsigned sizey)
+void iteration (double *g2, double *g1, unsigned sizex, unsigned sizey)
 {
 	double diff, sum=0.0;
 	int nbx, bx, nby, by;
@@ -8,7 +8,7 @@ void relax_jacobi (double *g2, double *g1, unsigned sizex, unsigned sizey)
 	nby = 2; 								// Nr de chunks por thread 					
 	by = sizey/nby;							// chunksize em cada coluna
 
-	#pragma omp parallel for reduction//(+:sum) private(diff)
+	#pragma omp parallel for //reduction(+:sum) private(diff)
 	//  i -> linhas; j colunas; ii-> chunk atual de linhas; jj-> chunk atual  de colunas
 	for (int ii=0; ii<nbx; ii++) { //Div das threads por linhas   
 		for (int jj=0; jj<nby; jj++)  { // Div das threads por colunas 
@@ -29,4 +29,22 @@ void relax_jacobi (double *g2, double *g1, unsigned sizex, unsigned sizey)
 	}
 
 //	return sum;
+}
+
+
+//faltam verificar as pontas
+void interation2 (double **g2, double **g1, unsigned sizex, unsigned sizey){
+#pragma omp parallel for 
+for (int i=1; i< sizey-2; ;i++){
+    for (int j=1; sizey-2 ; j++){
+    	linha=0;
+    	coluna=0;
+    	#pragma omp simd reduction (+:linha, +:coluna)
+    	for(int valx=i-1, int valy=j-1, a=0; a<3; a++){
+        coluna += g2[valx][j];
+        linha  += g2[i][valy];
+    }
+    sum [i][j] = 0.2*(linha + couluna - vec[i][j]) // -vec[i][j] retirar o elemento central da cruz repetido -> alternativa (1 if )
+    //coluna = linha=0;
+}
 }

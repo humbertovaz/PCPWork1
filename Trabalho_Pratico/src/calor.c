73836292952907f311e69a30c2dae30b811c7b2d
Humberto Vaz <a73236@alunos.uminho.pt>
2017-11-16 01:17:30 +0000

    Added calor.c

--- Trabalho_Pratico/src/calor.c
+++ Trabalho_Pratico/src/calor.c
@@ -3,12 +3,13 @@
 #include <time.h>
 #include <omp.h>
 #include <string.h>
-
-#define M 500
-#define N 500
+#define min(a,b) ( ((a) < (b)) ? (a) : (b) )
+#define M 10000
+#define N 10000
 #define ITER 5000
 double G1[N][N];
 double G2[N][N];
+double wtime=0;
 //int NMAXTHREADS=omp_get_max_threads();
 
 long long unsigned initial_time;
@@ -46,8 +47,9 @@ void fillMatrix(){
                 }
     }
 }
-void iterationSequential(){
+double iterationSequential(){
     int iter=0;
+    wtime = omp_get_wtime ();
     while (iter < ITER){
         for(int i=1; i<N-1; i++){ 
             for(int j=1; j<M-1; j++){
@@ -59,18 +61,48 @@ void iterationSequential(){
                     G2[i][j]);
             }   
         }
-        //Copy the G1 array calculated values back to the G2 array -> MEMCOPY
-        for(int i=0; i<N;i++){
-            for(int j=0;j<M;j++)
-                G2[i][j]=G1[i][j];
-        }
+        memcpy(G1,G2,N*M*sizeof(double));
+                
     ++iter;
-    } 
+    }
+return (omp_get_wtime() - wtime); 
 }
-void iterationParallel(){
+double iterationBlocks(){
+int nbx, bx, nby, by;
+  nbx = omp_get_max_threads();      // NR de threads 
+  bx = M/nbx + ((M%nbx) ? 1 : 0); // linha do bloco (quantas linhas fica cada thread)
+  nby = 1;                // Nr de chunks por thread          
+  by = N/nby;             // coluna do bloco (quantas tem cada bloco)
+  int iter=0;
+  wtime = omp_get_wtime();
+  
+    while(iter<ITER){
+        #pragma omp parallel for  //reduction(+:sum) private(diff) //  i -> linhas; j colunas; ii-> chunk atual de linhas; jj-> chunk atual  de colunas
+        for (int ii=0; ii<nbx; ii++)  // Criar #nbx threads    
+            for (int jj=0; jj<nby; jj++)   // Limitar chunksize por thread
+                for (int i=1+ii*bx; i<=min((ii+1)*bx, M-2); i++)       // cada i é uma linha do bloco
+                    for (int j=1+jj*by; j<=min((jj+1)*by, N-2); j++) {    // cada j é uma coluna do bloco
+                    G1[i][j]= 0.2 * (G2[i][j]+ //itself  
+                                    G2[i][j-1]+  // left
+                                    G2[i][j+1]+  // right
+                                    G2[(i-1)][j]+  // top
+                                    G2[(i+1)][j]); // bottom
+                    }
+                //Copiar de volta para a Memória
+            #pragma omp parallel for
+            for(int i=0; i<N;++i)
+                for(int j=0;j<M;++j)
+                    G2[i][j]=G1[i][j];
+
+    ++iter;
+    }
+return (omp_get_wtime() - wtime);
+}
+double iterationParallel(){
     int iter=0;
+    wtime = omp_get_wtime ();
     while (iter < ITER){
-        #pragma omp parallel for num_threads(8)
+        #pragma omp parallel for num_threads(omp_get_max_threads())
         for(int i=1; i<N-1; ++i){ 
             for(int j=1; j<M-1; ++j){
                 G1[i][j] = 0.2*(
@@ -84,72 +116,42 @@ void iterationParallel(){
     
     
     //Copy the G1 array calculated values back to the G2 array -> MEMCOPY
-        //#pragma omp parallel for
+        #pragma omp parallel for
         for(int i=0; i<N;++i){
             for(int j=0;j<M;++j)
                 G2[i][j]=G1[i][j];
         }
-        //memcpy(&G1,&G2,N*M*sizeof(double));
+        //memcpy(G1,G2,N*M*sizeof(double));
     ++iter;
     }
+return (omp_get_wtime () - wtime);   
 }
 int main(int argc, char const *argv[]){
-    int sequential=1,parallel=1;
+    int sequential=1,parallel=1,blocks=1;
     //clock_t start_t, end_t, total_t;
     //start_t=omp_get_wtime();
     double start,end,total;
 //#ifdef D_SEQUENTIAL
 if (sequential==1){
     clearCache();
-    fillMatrix ();
-    //Collects the start time
-    //start_t = omp_get_wtime();
-    start= omp_get_wtime();
-    //printf("Starting of the program, start_t = %ld\n", start_t);
-    printf("SEQUENTIAL-Starting of the program, start_t = %f\n", start);
-    // Does the job
-    iterationSequential();
-    //Collects the end time
-    //end_t = omp_get_wtime();
-    end=omp_get_wtime();
-    // Ends the job
-    //printf("End of the big loop, end_t = %ld\n", end_t);
-    printf("SEQUENTIAL-End of the big loop, end_t = %f\n", end);
-    //total_t = (double)(end_t - start_t) / CLOCKS_PER_SEC;
-    total = (double)(end - start);
-    //printf("Total time taken by CPU: %ld\n", total_t  );
+    fillMatrix();
+    total=iterationSequential();
     printf("SEQUENTIAL-Total time taken by CPU: %f\n", total  );
 }
     //#endif 
-
 //#ifdef D_PARALLEL
 if (parallel==1){
     clearCache();
-    fillMatrix ();
-    //Collects the start time
-    //start_t = clock();
-    start=omp_get_wtime();
-    //printf("Starting of the program, start_t = %ld\n", start_t);
-    printf("PARALLEL- Starting of the program, start_t = %f\n", start);
-    // Does the job
-    iterationParallel();
-    //Collects the end time
-    //end_t = clock();
-    end=omp_get_wtime();
-    // Ends the job
-    //printf("End of the big loop, end_t = %ld\n", end_t);
-    printf("PARALLEL- End of the big loop, end_t = %f\n", end);
-    //total_t = (double)(end_t - start_t) / CLOCKS_PER_SEC;
-    total=(double)(end - start) ;
-    //printf("Total time taken by CPU: %ld\n", total_t  );
+    fillMatrix();
+    total=iterationParallel();
     printf("PARALLEL- Total time taken by CPU: %f\n", total);
 }
     //#endif
-//Global Time
-//end_t=omp_get_wtime();
-//total_t=end_t-start_t;
-//printf("Total time of both Sequential and Parallel execution:%ld\n",total_t );    
-
-
-    
+if (blocks==1){
+    clearCache();
+    fillMatrix();
+    total=iterationBlocks();
+    printf("PARALLEL w/ Blocks- Total time taken by CPU: %f\n", total);
+}
+    printf("Done!\n");
 }

bd18d8fe963fba4c80a5b66d5012a97714f3fbe1
humbertovaz <humbertojoao13@hotmail.com>
2017-11-18 17:07:33 +0000

    src/calor.c with new cache friendly improvements

--- Trabalho_Pratico/src/calor.c
+++ Trabalho_Pratico/src/calor.c
@@ -51,16 +51,18 @@ void fillMatrix(){
 }
 double iterationSequential(){
     int iter=0;
+    double aux[M];
     wtime = omp_get_wtime ();
     while (iter < ITER){
         for(int i=1; i<N-1; i++){ 
+            aux = G2[i];
             for(int j=1; j<M-1; j++){
                 G1[i][j]=0.2*( 
                     G2[i-1][j]+
                     G2[i+1][j]+
-                    G2[i][j-1]+
-                    G2[i][j+1]+
-                    G2[i][j]);
+                    aux[j-1]+
+                    aux[j+1]+
+                    aux[j]);
             }   
         }
         memcpy(G1,G2,N*M*sizeof(double));
@@ -102,17 +104,19 @@ return (omp_get_wtime() - wtime);
 }
 double iterationParallel(){
     int iter=0;
+    double aux[M]
     wtime = omp_get_wtime ();
     while (iter < ITER){
-        #pragma omp parallel for num_threads(omp_get_max_threads())
+        #pragma omp parallel for num_threads(omp_get_max_threads()) private(aux)
         for(int i=1; i<N-1; ++i){ 
+            aux = G2[i];
             for(int j=1; j<M-1; ++j){
                 G1[i][j] = 0.2*(
                     G2[i-1][j]+
                     G2[i+1][j]+
-                    G2[i][j-1]+
-                    G2[i][j+1]+
-                    G2[i][j]);
+                    aux[j-1]+
+                    aux[j+1]+
+                    aux[j]);
             }
         }
     
@@ -120,8 +124,9 @@ double iterationParallel(){
     //Copy the G1 array calculated values back to the G2 array -> MEMCOPY
         #pragma omp parallel for
         for(int i=0; i<N;++i){
+            aux = G1[i];
             for(int j=0;j<M;++j)
-                G2[i][j]=G1[i][j];
+                G2[i][j]=aux[j];
         }
         //memcpy(G1,G2,N*M*sizeof(double));
     ++iter;

6c9e292620f893ed275b1d08a01ce9ecb024570a
João Tiago Pereira Dias <joaotpd099@gmail.com>
2017-11-20 16:34:23 +0000

    alterações feitas ao stencil

--- Trabalho_Pratico/src/pseudostencil.c
+++ Trabalho_Pratico/src/pseudostencil.c
@@ -31,7 +31,7 @@ double iterationParallel(){
 
         //colunas lado direito
         for(int j=1; j<M-1 && j <= iter+1; ++j){ //ver qual gasta menos < ou <=
-            for(int i=1; i<N-1; ++i){
+            for(int i=1+iter; i<N-1; ++i){ //1 ou 2?
                 G1[i][j] = 0.2*(
                     G2[i-1][j]+
                     G2[i+1][j]+
@@ -45,7 +45,7 @@ double iterationParallel(){
 
                 //linhas inferiores
         for(int j=M-1-(iter+1); j<M-1; ++j){ //ver qual gasta menos < ou <=
-            for(int i=1; i<N-1; ++i){
+            for(int i=1+iter; i<N-1; ++i){//1 ou 2
                 G1[i][j] = 0.2*(
                     G2[i-1][j]+
                     G2[i+1][j]+

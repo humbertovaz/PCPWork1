d8a7af8950fbdbb6d815921594dc705db3400e00
Humberto Vaz <a73236@alunos.uminho.pt>
2017-11-22 01:06:47 +0000

    Update run.sh

--- outputs/run.sh
+++ outputs/run.sh
@@ -1,21 +1,31 @@
 #!/bin/sh
 
-
-
-export ITER=2
-export THREADS=1
-export LINES=10
-export COLLUMNS=10
-
 # Mode 1 -> Sequential ITER; Mode 2 COSWAP; Mode 3 CopMem
-export MODE=1
-./Trabalho_Pratico/bin/heatplate $MODE $ITER $THREADS $LINES $COLLUMNS
+
+module load gcc/5.3.0
+
+echo "Running.."
+
+echo "A compilar ficheiros.."
+
+cd /home/a73236/PCP/PCPWork1/
+gcc Trabalho_Pratico/src/heatplate.c -O3 -fopenmp -Wall -Wextra -std=c99 -finline -Wno-unused-parameter -o /home/a73236/PCP/PCPWork1/Trabalho_Pratico/bin/heatplate
+
+echo "A correr "
+
+cd Trabalho_Pratico/bin
+            #$MODE $ITER $THREADS $LINES $COLLUMNS        
 
 
-export MODE=2
+echo ""
+./heatplate 1 1 1 10000 10000
+echo ""
+./heatplate 1 1 1 20000 20000
+echo ""
+./heatplate 1 1 1 30000 30000
+echo ""
+./heatplate 1 1 1 40000 40000
+echo ""
+./heatplate 1 1 1 50000 50000
 
-./Trabalho_Pratico/bin/heatplate $MODE $ITER $THREADS $LINES $COLLUMNS
-
-export MODE=3
-
-./Trabalho_Pratico/bin/heatplate $MODE $ITER $THREADS $LINES $COLLUMNS
+ 

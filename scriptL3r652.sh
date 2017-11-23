5a919ef5c3503d185c5b8c1a88832f93ab433bd7
humbertovaz <humbertojoao13@hotmail.com>
2017-11-23 19:48:04 +0000

    Added scripts module load to the scripts v2

--- scriptL3r652.sh
+++ scriptL3r652.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export ITER=1
-export LINES=10
-export COLLUMNS=10
+export LINES=400000
+export COLLUMNS=400000
 module load gcc/4.9.0
 gcc Trabalho_Pratico/src/heatplate.c -O3 -fopenmp -Wall -Wextra -std=c99 -finline -Wno-unused-parameter -o Trabalho_Pratico/bin/heatplate
 
